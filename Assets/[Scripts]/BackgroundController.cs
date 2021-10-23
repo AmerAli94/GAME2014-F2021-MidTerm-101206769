93cbcfcaa33d6edb6f1265c458aa7a9dc620456b
Amer Ali <ameralizee93@gmail.com>
2021-10-22 20:04:09 -0400

    Changed the project to landscape and added right to left background scrolling.

--- Assets/[Scripts]/BackgroundController.cs
+++ Assets/[Scripts]/BackgroundController.cs
@@ -1,11 +1,26 @@
-﻿using System.Collections;
+﻿// ===============================
+// PROGRAM NAME: GAME Programming (T163)
+// STUDENT ID : 101206769
+// AUTHOR     : AMER ALI MOHAMMED
+// CREATE DATE     : OCT 22, 2021
+// PURPOSE     : GAME2014_F2021_MIDTERM_101206769
+// SPECIAL NOTES:
+// ===============================
+// Change History:
+// Changed the background scroll to landscape right to left scroll.
+//==================================
+//
+//==================================
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BackgroundController : MonoBehaviour
 {
-    public float verticalSpeed;
-    public float verticalBoundary;
+    //changed vertical to horizontal speed.
+    public float horizontalSpeed;
+    public float horizontalBoundary;
 
     // Update is called once per frame
     void Update()
@@ -16,18 +31,19 @@ public class BackgroundController : MonoBehaviour
 
     private void _Reset()
     {
-        transform.position = new Vector3(0.0f, verticalBoundary);
+        transform.position = new Vector3( 21.0f, 0.0f);
+        
     }
 
     private void _Move()
     {
-        transform.position -= new Vector3(0.0f, verticalSpeed) * Time.deltaTime;
+        transform.position -= new Vector3(horizontalSpeed, 0.0f) * Time.deltaTime;
     }
 
     private void _CheckBounds()
     {
         // if the background is lower than the bottom of the screen then reset
-        if (transform.position.y <= -verticalBoundary)
+        if (transform.position.x <= horizontalBoundary)
         {
             _Reset();
         }
