2ac990c3575f2948db1d0abce703861c564b9026
Amer Ali <ameralizee93@gmail.com>
2021-10-24 20:25:45 -0400

    Fixed the enemy movement to top-down.

--- Assets/[Scripts]/EnemyController.cs
+++ Assets/[Scripts]/EnemyController.cs
@@ -1,19 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
     public Transform enemyTransform;
-    public float horizontalSpeed;
-    public float horizontalBoundary;
+    public float verticalSpeed;
+    public float verticalBoundary;
     public float direction;
 
     private void Start()
     {
         enemyTransform = GetComponent<Transform>();
        // enemyTransform.transform.position = new Vector3(7.6f, 0.0f, 0.0f);
-        enemyTransform.transform.position = new Vector3(8.7f, Random.Range(-4.55f, 4.55f), 0.0f);
+        enemyTransform.transform.position = new Vector3(8.0f, Random.Range(-4.55f, 4.55f), 0.0f);
         enemyTransform.transform.Rotate(0.0f, 0.0f, 270.0f);
     }
 
@@ -27,19 +25,20 @@ public class EnemyController : MonoBehaviour
 
     private void _Move()
     {
-        transform.position += new Vector3(horizontalSpeed * direction * Time.deltaTime, 0.0f, 0.0f);
+        //transform.position += new Vector3(horizontalSpeed * direction * Time.deltaTime, 0.0f, 0.0f);
+        transform.position += new Vector3(0.0f, verticalSpeed * direction * Time.deltaTime, 0.0f); // moving enemies on y axis for top down movement
     }
 
     private void _CheckBounds()
     {
         // check right boundary
-        if (transform.position.x >= horizontalBoundary)
+        if (transform.position.y >= verticalBoundary) //bounds moved to y axis
         {
             direction = -1.0f;
         }
 
         // check left boundary
-        if (transform.position.x <= -horizontalBoundary)
+        if (transform.position.y <= -verticalBoundary) //bounds moved to y axis
         {
             direction = 1.0f;
         }
